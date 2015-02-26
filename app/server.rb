944eae94a919bf2a72c2f746375a5da1737c3995
Stephen Giles <steve@steve-mac.local>
2015-02-26 08:41:44 +0000

    First commit

---
+++ app/server.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'sinatra/partial'
+
+set :partial_template_engine, :erb
+set :public_folder, Proc.new { File.join(root, '..', 'public') }
+
+get '/' do
+  erb :index
+end
+
+get '/basics' do
+  erb :basics
+end
